f7f9d22facf393f79d2f085dbb133dfdd49195a5
John Francis <john9francis@gmail.com>
2023-01-05 23:39:50 -0700

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+        bool goodMessage = false;
+
+        Console.Write("What is your grade percentage? ");
+        int per = int.Parse(Console.ReadLine());
+        if (per >= 90)
+        {
+            letter = "A";
+            goodMessage = true;
+        }
+        else if (per >= 80)
+        {
+            letter = "B";
+            goodMessage = true;
+        }
+        else if (per >= 70)
+        {
+            letter = "C";
+            goodMessage = true;
+        }
+        else if (per >= 60)
+        {
+            letter = "D";
+            goodMessage = false;
+        }
+        else
+        {
+            letter = "F";
+            goodMessage = false;
+        }
+
+        // check if it's a + or - (STRETCH CHALLANGE 1);
+        int lastDigit = per % 10;
+        string sign = "";
+        if (lastDigit >= 7)
+        {
+        
+        // preventing the A+ grade
+        if (letter != "A")
+            {
+                sign = "+";
+            }
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        // preventing F+ or F- grades
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        // display the grade and the good or bad message;
+        Console.WriteLine($"Grade: {letter}{sign}");
+        if (goodMessage == true)
+        {
+            Console.WriteLine("Great job!");
+        }
+        else 
+        {
+            Console.WriteLine("Better luck next time");
+        }
+        }
+        
     }
-}
