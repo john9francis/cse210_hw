49614ddb05f0ec98f2e81a5989dede01b66cb67c
John Francis <john9francis@gmail.com>
2023-01-11 13:53:25 -0700

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //getting random number;
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+        int guesses = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > num)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < num)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == num)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            guesses += 1;
+        } while (guess != num);
+
+        // stretch challenge 1;
+        Console.WriteLine($"Guesses: {guesses}");
+        
     }
 }
