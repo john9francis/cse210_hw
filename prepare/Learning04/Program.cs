ebbb0a03ee9c903126e95b015164bbbabbafb844
John Francis <john9francis@gmail.com>
2023-02-16 11:39:27 -0700

    testing

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        // testing git;
+        Console.WriteLine("testing Git");
     }
 }
